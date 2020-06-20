da9fc59372462739ad6f84eb65baf8e00f09410e
yyqlovelife <50950108+yyqlovelife@users.noreply.github.com>
2020-06-20 17:52:04 +0800

    Add files via upload

---
+++ src/test/java/com/scut/blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,20 @@
+package com.scut.blockchain;
+
+import org.fisco.bcos.channel.client.Service;
+import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigInteger;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+    @Test
+    void contextLoads() throws Exception {
+    }
+
+}
